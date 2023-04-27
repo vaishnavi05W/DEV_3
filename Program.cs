049160c5269c79ef9145bd58a4f4698e4ce78590
vaishnavi05W <118710453+vaishnavi05W@users.noreply.github.com>
2023-04-27 16:22:43 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vdControls;
+using OpenVDF;
+using VectorDraw;
+using VectorDraw.Geometry;
+using VectorDraw.Professional.Constants;
+using VectorDraw.Professional.vdCollections;
+using VectorDraw.Professional.vdPrimaries;
+using VectorDraw.Actions;
+using SpaceLayout.Forms.ZoneForms;
+
+
+namespace SpaceLayout
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFirstPageControl());
+        }
+    }
+}
+ 

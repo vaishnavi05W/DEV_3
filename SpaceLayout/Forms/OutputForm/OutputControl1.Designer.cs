8fd32ac406e9ddd5837a7dbc367e7f5283dd294d
Saw Yupar Kyaw <sawyuparkyaw2294@gmail.com>
2023-06-18 23:31:59 +0630

    by saw lay develop excel export function

--- SpaceLayout/Forms/OutputForm/OutputControl1.Designer.cs
+++ SpaceLayout/Forms/OutputForm/OutputControl1.Designer.cs
@@ -31,8 +31,8 @@ namespace SpaceLayout.Forms.ZoneForms
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
@@ -50,13 +50,13 @@ namespace SpaceLayout.Forms.ZoneForms
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.ForeColor = System.Drawing.Color.White;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.57025F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.00264F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.519789F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(512, 758);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.89182F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.630607F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(512, 746);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -64,42 +64,42 @@ namespace SpaceLayout.Forms.ZoneForms
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnExport, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnFinish, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 704);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 677);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(504, 50);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(504, 65);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // button1
+            // btnExport
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.ForeColor = System.Drawing.Color.Navy;
-            this.button1.Location = new System.Drawing.Point(7, 6);
-            this.button1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(238, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Export";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnExport.BackColor = System.Drawing.Color.White;
+            this.btnExport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExport.ForeColor = System.Drawing.Color.Navy;
+            this.btnExport.Location = new System.Drawing.Point(7, 6);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(238, 53);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnFinish
             // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.ForeColor = System.Drawing.Color.Navy;
-            this.button2.Location = new System.Drawing.Point(259, 6);
-            this.button2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(238, 38);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Finish";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnFinish.BackColor = System.Drawing.Color.White;
+            this.btnFinish.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnFinish.ForeColor = System.Drawing.Color.Navy;
+            this.btnFinish.Location = new System.Drawing.Point(259, 6);
+            this.btnFinish.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(238, 53);
+            this.btnFinish.TabIndex = 1;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = false;
             // 
             // checkedListBox1
             // 
@@ -109,10 +109,10 @@ namespace SpaceLayout.Forms.ZoneForms
             "관계도 다이어그램 .dwg",
             "관계도 다이어그램 .jpg",
             "관계도 .xlsx"});
-            this.checkedListBox1.Location = new System.Drawing.Point(4, 91);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkedListBox1.Location = new System.Drawing.Point(4, 90);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(504, 605);
+            this.checkedListBox1.Size = new System.Drawing.Size(504, 579);
             this.checkedListBox1.TabIndex = 1;
             // 
             // textBox1
@@ -121,7 +121,7 @@ namespace SpaceLayout.Forms.ZoneForms
             this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.Navy;
             this.textBox1.Location = new System.Drawing.Point(4, 27);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(504, 32);
             this.textBox1.TabIndex = 2;
@@ -132,9 +132,9 @@ namespace SpaceLayout.Forms.ZoneForms
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "OutputControl1";
-            this.Size = new System.Drawing.Size(512, 758);
+            this.Size = new System.Drawing.Size(512, 746);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
@@ -146,8 +146,8 @@ namespace SpaceLayout.Forms.ZoneForms
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnFinish;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.TextBox textBox1;
     }
--- SpaceLayout/Forms/OutputForm/OutputControl1.cs
+++ SpaceLayout/Forms/OutputForm/OutputControl1.cs
@@ -7,20 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Office.Interop;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace SpaceLayout.Forms.ZoneForms
 {
     public partial class OutputControl1 : UserControl
     {
+        private DataTable dtExcelExport = new DataTable();
         public OutputControl1(DataTable dtZoneRelationshipControl)
         {
             InitializeComponent();
             this.Load += IS_Load;
+            dtExcelExport = dtZoneRelationshipControl;
         }
 
         private void IS_Load(object sender, EventArgs e)
         {
+            btnExport.Click += btnExport_Clicked;
+        }
 
+        private void btnExport_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkedListBox1.CheckedItems.Count > 0)
+                {
+                    string s = "";
+                    for (int x = 0; x < checkedListBox1.CheckedItems.Count; x++)
+                    {
+                        if(checkedListBox1.CheckedItems[x].ToString() == "관계도 .xlsx")
+                        {
+                            if (dtExcelExport.Rows.Count > 0)
+                            {
+                                string folderPath = "C:\\temp\\";
+                                if (!Directory.Exists(folderPath))
+                                {
+                                    Directory.CreateDirectory(folderPath);
+                                }
+                                SaveFileDialog savedialog = new SaveFileDialog();
+                                savedialog.Filter = "Excel Files|*.xlsx;";
+                                savedialog.Title = "Save";
+                                savedialog.FileName = "관계도";
+                                savedialog.InitialDirectory = folderPath;
+
+                                savedialog.RestoreDirectory = true;
+
+                                if (savedialog.ShowDialog() == DialogResult.OK)
+                                {
+                                    if (Path.GetExtension(savedialog.FileName).Contains(".xlsx"))
+                                    {
+                                        ExportDataTableToExcel(dtExcelExport, savedialog.FileName);
+                                        Process.Start(Path.GetDirectoryName(savedialog.FileName));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                   
+                }
+                else
+                {
+                    MessageBox.Show("Please check at least one item.");
+                    return;
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+       
+        public static bool ExportDataTableToExcel(DataTable dt, string filepath)
+        {
+            Excel.Application oXL;
+            Excel.Workbook oWB;
+            Excel.Worksheet oSheet;
+            Excel.Range oRange;
+
+            try
+            {
+                // Start Excel and get Application object. 
+                oXL = new Excel.Application();
+
+                // Set some properties 
+                oXL.Visible = true;
+                oXL.DisplayAlerts = false;
+
+                // Get a new workbook. 
+                oWB = oXL.Workbooks.Add(System.Reflection.Missing.Value);
+
+                // Get the Active sheet 
+                oSheet = (Excel.Worksheet)oWB.ActiveSheet;
+                oSheet.Name = "Data";
+
+                int rowCount = 1;
+                foreach (DataRow dr in dt.Rows)
+                {
+                    rowCount += 1;
+                    for (int i = 1; i < dt.Columns.Count + 1; i++)
+                    {
+                        // Add the header the first time through 
+                        if (rowCount == 2)
+                        {
+                            oSheet.Cells[1, i] = dt.Columns[i - 1].ColumnName;
+                        }
+                        oSheet.Cells[rowCount, i] = dr[i - 1].ToString();
+                    }
+                }
+
+                // Save the sheet and close 
+                oSheet = null;
+                oRange = null;
+                oWB.SaveAs(filepath);
+                oWB = null;
+                oXL.Quit();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                // Clean up 
+                // NOTE: When in release mode, this does the trick 
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+            }
+
+            return true;
         }
     }
 }

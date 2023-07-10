b6a16d90ec5683a6d78131de9da7701b604bf45a
vaishnavi <vaishnavi@wyzrs.com>
2023-07-10 19:09:25 +0900

    VN- Buttons

--- SpaceLayout/Forms/OutputForm/GridViewTest.cs
+++ SpaceLayout/Forms/OutputForm/GridViewTest.cs
@@ -21,18 +21,88 @@ namespace SpaceLayout.Forms.OutputForm
         private void IS_Load(object sender, EventArgs e)
         {
             dataGridView1.AutoGenerateColumns = false;
-            
+
             DataTable dataTable = new DataTable();
 
-            dataTable.Columns.Add("Column1", typeof(Button));
-            dataTable.Columns.Add("Column2", typeof(Button));
-            dataTable.Columns.Add("Column3", typeof(Button));
+            //dataTable.Columns.Add("Column1", typeof(Button));
+            //dataTable.Columns.Add("Column2", typeof(Button));
+            //dataTable.Columns.Add("Column3", typeof(Button));
             int buttonCount = 16; // Number of buttons to generate
+            int buttonsPerRow = 3;
+            int rowCount = (int)Math.Ceiling((double)buttonCount / buttonsPerRow);
 
-            for (int s = 0; s < buttonCount; s++)
+            for (int row = 0; row < rowCount; row++)
             {
-                dataTable.Rows.Add(new Button(), new Button(), new Button());
+                DataGridViewRow dataGridViewRow = new DataGridViewRow();
+                dataGridViewRow.CreateCells(dataGridView1);
+
+                for (int col = 0; col < buttonsPerRow; col++)
+                {
+                    int buttonIndex = (row * buttonsPerRow) + col;
+                    if (buttonIndex < buttonCount)
+                    {
+                        Button button = new Button();
+                        button.Text = "Set Text";
+                        dataGridViewRow.Cells[col] = new DataGridViewButtonCell()
+                        {
+                            Value = button
+                        };
+                    }
+                }
+
+                dataGridView1.Rows.Add(dataGridViewRow);
             }
+            //    for (int col = 1; col <= buttonsPerRow; col++)
+            //{
+            //    DataGridViewTextBoxColumn column = new DataGridViewTextBoxColumn
+            //    {
+            //        Name = "Column" + col,
+            //        HeaderText = "Column" + col,
+
+            //    };
+
+            //    dataGridView1.Columns.Add(column);
+            //}
+            
+            //for (int row = 0; row < rowCount; row++)
+            //{
+            //    DataGridViewRow dataGridViewRow = new DataGridViewRow();
+            //    dataGridViewRow.CreateCells(dataGridView1);
+
+            //    for (int col = 0; col < buttonsPerRow; col++)
+            //    {
+            //        int buttonIndex = (row * buttonsPerRow) + col;
+            //        if (buttonIndex < buttonCount)
+            //        {
+            //            dataGridViewRow.Cells[col].Value = "Set Text";
+            //        }
+            //    }
+
+            //    dataGridView1.Rows.Add(dataGridViewRow);
+            //}
+
+            //for (int i = 1; i <= buttonsPerRow; i++)
+            //{
+            //    dataTable.Columns.Add("Column" + i, typeof(Button));
+            //}
+
+            //for (int row = 0; row < rowCount; row++)
+            //{
+            //    DataRow dataRow = dataTable.NewRow();
+            //    for (int col = 0; col < buttonsPerRow; col++)
+            //    {
+            //        int buttonIndex = (row * buttonsPerRow) + col;
+            //        if (buttonIndex < buttonCount)
+            //        {
+            //            dataRow[col] = new Button();
+            //        }
+            //    }
+            //    dataTable.Rows.Add(dataRow);
+            //}
+            //for (int s = 0; s < buttonCount; s++)
+            //{
+            //    dataTable.Rows.Add(new Button(), new Button(), new Button());
+            //}
             //int s = 1;
             //do
             //{
@@ -47,15 +117,19 @@ namespace SpaceLayout.Forms.OutputForm
 
             //dataTable.Rows.Add(new Button() ,new Button() , new Button() );
 
-            dataGridView1.DataSource = dataTable;
+            //dataGridView1.DataSource = dataTable;
 
-            foreach (DataGridViewRow row in dataGridView1.Rows)
-            {
-                DataGridViewCell cell = row.Cells[0];//Column Index
-                cell.Value = "Set Text";
-            }
+            //foreach (DataGridViewRow row in dataGridView1.Rows)
+            //{
+            //    foreach (DataGridViewCell dataGridViewCell in row.Cells)
+            //    {
+            //        if (dataGridViewCell.Value is Button button)
+            //        {
+            //            button.Text = "Set Text";
+            //        }
+            //    }
+            //}
         }
-
         private void GridViewTest_Load(object sender, EventArgs e)
         {
 
